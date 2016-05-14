aa041d7f8037633c455d57c3297777d89138a06d
gabriel <gabriel.meira.25@gmail.com>
2016-05-13 23:19:12 -0300

    Nested Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :travels
-  resources :toll_boths
-  resources :fuels
-  resources :meals
-  resources :lodgings
-  resources :travels
-  resources :toll_booths
-  resources :fuels
-  resources :meals
-  resources :lodgings
-  resources :travels
+  resources :travels do
+    resources :toll_boths
+    resources :fuels
+    resources :meals
+    resources :lodgings
+  end
+
   resources :vehicles
   resources :cities
   resources :states
+
   root to: 'visitors#index'
+
   devise_for :users
+
   get 'contact/index'
 end
