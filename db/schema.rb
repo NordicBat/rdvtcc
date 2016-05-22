dff3cd8c84d4fc366c07feb618e637b7d6e182f2
gabriel <gabriel.meira.25@gmail.com>
2016-05-22 14:48:01 -0300

    ImageUplooader:  Meals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521003442) do
+ActiveRecord::Schema.define(version: 20160522173054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160521003442) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "travel_id"
+    t.string   "image"
   end
 
   add_index "meals", ["travel_id"], name: "index_meals_on_travel_id", using: :btree
