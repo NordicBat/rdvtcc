af80b13ed771e42bcfec85e33707543311721a44
gabriel <gabriel.meira.25@gmail.com>
2016-05-30 21:28:57 -0300

    Travel Total amount now calculating properly

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -7,5 +7,16 @@ class Travel < ActiveRecord::Base
   has_many :toll_boths
   accepts_nested_attributes_for :meals, :lodgings, :fuels, :toll_boths
   has_one :vehicle
+  def total_meal_amount
+    meals.map(&:total).sum
+  end
+
+  def total_fuel_amount
+    fuels.map(&:total).sum
+  end
+
+  def total_lodging_amount
+    lodgings.map(&:total).sum
+  end
 
 end
